e0500a84b3196a0cda0b1849b1adbe768715030b
Stanislaw Jablonski <104526572+StanislawJablonski@users.noreply.github.com>
2023-07-25 12:14:11 +0200

    Vendor profile form + validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 
-from accounts.models import User
+from accounts.models import User, UserProfile
+from .validators import allowOnlyImagesValidator
 
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     confirm_password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
@@ -20,3 +22,22 @@ class UserForm(forms.ModelForm):
                 "Passwords don't match!"
             )
 
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Start typing...','required':'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),validators=[allowOnlyImagesValidator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),validators=[allowOnlyImagesValidator])
+
+    #latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    #longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city',
+                  'pin_code', 'latitude', 'longitude']
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args,**kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
--- vendor/forms.py
+++ vendor/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 
+from accounts.validators import allowOnlyImagesValidator
 from vendor.models import Vendor
 
 
 class VendorForm(forms.ModelForm):
+    vendor_license = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),validators=[allowOnlyImagesValidator])
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
