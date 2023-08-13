c2dcb897bb7cf4a8b66059ae49a7ef4ace10ed94
Stanislaw Jablonski <104526572+StanislawJablonski@users.noreply.github.com>
2023-08-13 13:38:10 +0200

    restaurant-detail view

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,10 @@
 from django.db.models import Prefetch
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from marketplace.context_processors import get_cart_counter
+from marketplace.models import Cart
 from menu.models import Category, FoodItem
 from vendor.models import Vendor
 
@@ -22,13 +25,71 @@ def vendor_detail(request, vendor_slug):
     categories = Category.objects.filter(vendor=vendor).prefetch_related(
         Prefetch(
             'fooditems',
-            queryset= FoodItem.objects.filter(is_available=True),
+            queryset=FoodItem.objects.filter(is_available=True),
         )
     )
 
-
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+    else:
+        cart_items = None
     context = {
-        'vendor':vendor,
-        'categories':categories,
+        'vendor': vendor,
+        'categories': categories,
+        'cart_items': cart_items,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
+
+
+def add_to_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':  # request.is_ajax() is deprecated
+            # check if fooditem exists
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user have already added food to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    # Increase the cart qtty
+                    chkCart.quantity += 1
+                    chkCart.save()
+                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity',
+                                         'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
+                except:
+                    chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
+                    return JsonResponse({'status': 'Success', 'message': 'Added food to the cart',
+                                         'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
+            except:
+                JsonResponse({'status': 'Failed', 'message': 'This food does not exist'})
+        else:
+            JsonResponse({'status': 'Failed', 'message': 'Invalid request'})
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
+
+
+def decrease_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':  # request.is_ajax() is deprecated
+            # check if fooditem exists
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user have already added food to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    # Decrease the cart qtty
+                    if chkCart.quantity >= 1:
+                        chkCart.quantity -= 1
+                        chkCart.save()
+                    else:
+                        chkCart.delete()
+                        chkCart.quantity = 0
+                    return JsonResponse(
+                        {'status': 'Success', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
+                except:
+                    return JsonResponse({'status': 'Failed', 'message': 'You do not have this item in cart'})
+            except:
+                JsonResponse({'status': 'Failed', 'message': 'This food does not exist'})
+        else:
+            JsonResponse({'status': 'Failed', 'message': 'Invalid request'})
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
